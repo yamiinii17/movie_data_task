49821b3fc4412e079f812783ac14c8a8700584b2
yamiinii17 <yaminipalandurkar30@gmail.com>
2025-09-14 17:25:36 +0530

    movie data

---
+++ movie_data task.sql
@@ -0,0 +1,525 @@
+create table moviedata (
+id serial primary key,
+name varchar,
+premiere date 
+)
+
+select * from moviedata
+
+insert into moviedata(id, name, premiere)
+values (00001, 'Pathaan', '1-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00002, 'Jawan', '2-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00003, 'Tiger 3', '3-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00004, 'Rocky Aur Rani Ki Prem Kahani', '4-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00005, 'Brahmastra', '5-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00006, 'Gadar 2', '6-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00007, 'Shershaah', '7-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00008, 'Sooryavanshi', '8-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00009, '83', '9-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00010, 'Satyaprem Ki Katha', '10-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00011, 'Bhool Bhulaiyaa 2', '11-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00012, 'Drishyam 2', '12-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00013, 'Laal Singh Chaddha', '13-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00014, 'Tu Jhoothi Main Makkaar', '14-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00015, 'Animal', '15-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00016, 'Adipurush', '16-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00017, 'Pushpa 2', '17-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00018, 'KGF Chapter 3', '18-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00019, 'Fighter', '19-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00020, 'Bade Miyan Chote Miyan', '20-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00021, 'Dunki', '21-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00022, 'Sam Bahadur', '22-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00023, 'OMG 2', '23-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00024, 'Dream Girl 2', '24-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00025, 'Jersey', '25-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00026, 'Gangubai Kathiawadi', '26-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00027, 'The Kashmir Files', '27-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00028, 'Kalank', '28-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00029, 'Raazi', '29-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00030, 'Uri: The Surgical Strike', '30-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00031, 'Andhadhun', '31-1-2000');
+
+insert into moviedata(id, name, premiere)
+values (00032, 'Gully Boy', '1-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00033, 'Article 15', '2-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00034, 'Super 30', '3-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00035, 'Kesari', '4-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00036, 'Chhichhore', '5-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00037, 'War', '6-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00038, 'Mission Mangal', '7-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00039, 'Batla House', '8-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00040, 'Tanhaji', '9-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00041, 'Good Newwz', '10-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00042, 'Ludo', '11-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00043, 'Bell Bottom', '12-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00044, 'Shubh Mangal Zyada Saavdhan', '13-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00045, 'Stree', '14-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00046, 'Bala', '15-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00047, 'Raabta', '16-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00048, 'Barfi!', '17-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00049, 'Kabir Singh', '18-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00050, 'Student of the Year 2', '19-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00051, 'Tamasha', '20-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00052, 'Padmaavat', '21-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00053, 'Dilwale Dulhania Le Jayenge', '22-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00054, '3 Idiots', '23-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00055, 'Zindagi Na Milegi Dobara', '24-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00056, 'Chennai Express', '25-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00057, 'Kabhi Khushi Kabhie Gham', '26-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00058, 'Dil Chahta Hai', '27-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00059, 'Lagaan', '28-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00060, 'Barfi!', '29-2-2000');
+
+insert into moviedata(id, name, premiere)
+values (00061, 'PK', '1-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00062, 'Bajrangi Bhaijaan', '2-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00063, 'Dangal', '3-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00064, 'Talaash', '4-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00065, 'Chhichhore', '5-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00066, 'Rockstar', '6-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00067, 'Bhaag Milkha Bhaag', '7-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00068, 'Jab We Met', '8-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00069, 'Raazi', '9-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00070, 'Barfi!', '10-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00071, 'Tanu Weds Manu', '11-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00072, 'Swades', '12-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00073, 'Zindagi Na Milegi Dobara', '13-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00074, 'PK', '14-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00075, '3 Idiots', '15-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00076, 'Dangal', '16-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00077, 'Queen', '17-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00078, 'Padmaavat', '18-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00079, 'Bajrangi Bhaijaan', '19-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00080, 'Andaz Apna Apna', '20-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00081, 'Mughal-e-Azam', '21-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00082, 'Kabhi Khushi Kabhie Gham', '22-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00083, 'Lagaan', '23-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00084, 'Sholay', '24-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00085, 'Don', '25-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00086, 'Kick', '26-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00087, 'Ek Tha Tiger', '27-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00088, 'Bhaag Johnny', '28-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00089, 'Talaash', '29-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00090, 'Kahaani', '30-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00091, 'Pink', '31-3-2000');
+
+insert into moviedata(id, name, premiere)
+values (00092, 'Drishyam', '1-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00093, 'Raajneeti', '2-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00094, 'Dhoom', '3-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00095, 'Dhoom 2', '4-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00096, 'Dhoom 3', '5-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00097, 'Kick', '6-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00098, 'Ra.One', '7-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00099, 'Sultan', '8-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00100, 'Tiger Zinda Hai', '9-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00101, 'War', '10-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00102, 'Bhootnath', '11-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00103, 'PK', '12-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00104, 'Krrish', '13-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00105, 'Krrish 3', '14-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00106, 'Jodhaa Akbar', '15-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00107, 'Ghajini', '16-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00108, 'Raanjhanaa', '17-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00109, 'Piku', '18-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00110, 'Tamasha', '19-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00111, 'Highway', '20-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00112, 'Udaan', '21-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00113, 'Airlift', '22-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00114, 'Haider', '23-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00115, 'Queen', '24-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00116, 'Drishyam 2', '25-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00117, 'Badhaai Ho', '26-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00118, 'Andhadhun', '17-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00119, 'Article 15', '18-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00120, 'Stree', '19-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00121, 'Ludo', '20-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00122, 'Gully Boy', '21-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00123, 'Tamasha', '22-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00124, 'Secret Superstar', '23-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00125, 'Bajrangi Bhaijaan', '24-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00126, 'Bareilly Ki Barfi', '25-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00127, 'Chhapaak', '26-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00128, 'Kesari', '27-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00129, 'Simmba', '28-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00130, 'Article 15', '29-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00131, 'Bhoot Part One', '30-4-2000');
+
+insert into moviedata(id, name, premiere)
+values (00132, 'Rashmi Rocket', '1-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00133, 'Shershaah', '2-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00134, 'Bhuj: The Pride of India', '3-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00135, 'Love Aaj Kal', '4-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00136, 'Pati Patni Aur Woh', '5-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00137, 'Kesari', '6-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00138, 'Chhichhore', '7-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00139, 'Satyameva Jayate', '8-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00140, 'Tanhaji', '9-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00141, 'Raanjhanaa', '10-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00142, 'Barfi!', '11-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00143, 'Kick', '12-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00144, 'Dhoom 3', '13-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00145, 'PK', '14-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00146, 'Bajirao Mastani', '15-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00147, 'Padmaavat', '16-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00148, 'Gully Boy', '17-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00149, 'Secret Superstar', '18-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00150, 'Raazi', '19-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00151, 'Queen', '20-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00152, 'Andhadhun', '21-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00153, 'Article 15', '22-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00154, 'Drishyam', '23-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00155, 'Piku', '24-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00156, 'Bajrangi Bhaijaan', '25-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00157, 'Taare Zameen Par', '26-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00158, 'Swades', '27-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00159, 'Lagaan', '28-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00160, '3 Idiots', '29-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00161, 'Chhichhore', '30-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00162, 'Rockstar', '31-5-2000');
+
+insert into moviedata(id, name, premiere)
+values (00163, 'Bhaag Milkha Bhaag', '1-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00164, 'Jab We Met', '2-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00165, 'Raazi', '3-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00166, 'Barfi!', '4-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00167, 'Tanu Weds Manu', '5-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00168, 'Swades', '6-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00169, 'Zindagi Na Milegi Dobara', '7-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00170, 'PK', '8-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00171, '3 Idiots', '9-6-2000');
+
+insert into moviedata(id, name, premiere)
+values (00172, 'Dangal', '10-6-2000');
+
+select * from moviedata
